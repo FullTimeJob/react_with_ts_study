083d243085238e465598d1a0001b7c1d53fcbf51
whiteswear1 <whiteswear1@gmail.com>
2022-09-18 17:18:24 +0900

    Add TestPopUp

--- src/pages/Gyusik/index.tsx
+++ src/pages/Gyusik/index.tsx
@@ -1,10 +1,70 @@
 import React from "react"
 import styled from "styled-components"
 
-const SGrid = styled.div`
+const SPage = styled.div`
+	display: flex;
 	width: 100%;
+	height: ${() => window.innerHeight}px;
+	text-align: center;
+	align-items: center;
+	justify-content: center;
+`
+
+const SContents = styled.div`
+	display: flex;
+	flex-direction: column;
+	background-color: grey;
+	height: 300px;
+	width: 400px;
+	justify-content: center;
+	align-items: center;
+`
+const SText = styled.div`
+	margin: 0 0 30px 0;
+`
+
+const SButtonWrapper = styled.div`
+	display: flex;
+`
+
+const SButton = styled.div`
+	display: flex;
+	width: 100px;
+	height: 35px;
+	color: white;
+	background-color: red;
+	margin: 0 30px 0 0;
+	align-items: center;
+	justify-content: center;
+`
+const SButton2 = styled.div`
+	display: flex;
+	width: 100px;
+	height: 35px;
+	color: white;
+	background-color: red;
+	margin: 0 0 0 0;
+	align-items: center;
+	justify-content: center;
 `
 
 export default function Gyusik() {
-	return <SGrid>규식</SGrid>
+	const handleClick1 = () => {
+		alert("당신은 누구입니까?")
+	}
+
+	const handleClick2 = () => {
+		alert("반갑습니다.")
+	}
+	return (
+		<SPage>
+			<SContents>
+				<SText>저장하시겠습니까?</SText>
+				<SButtonWrapper>
+					<SButton onClick={handleClick1}>확인</SButton>
+					<SButton2 onClick={handleClick2}>취소</SButton2>
+				</SButtonWrapper>
+			</SContents>
+		</SPage>
+	)
 }
