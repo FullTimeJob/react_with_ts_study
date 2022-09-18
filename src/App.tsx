254cb91de2bcda6746c068aaac3861b84ede4468
whiteswear1 <whiteswear1@gmail.com>
2022-09-18 13:23:50 +0900

    Dividing UI for sharework

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 import React from "react"
+import styled from "styled-components"
+import Gyusik from "./pages/Gyusik"
+import Heesang from "./pages/Heesang"
+
+const SGrid = styled.div`
+	display: flex;
+`
 
 export default function App() {
-	return <div className="App">k</div>
+	return (
+		<SGrid>
+			<Gyusik />
+			<Heesang />
+		</SGrid>
+	)
 }
---
+++ src/pages/Heesang/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import styled from "styled-components"
+
+const SGrid = styled.div`
+	width: 100%;
+`
+
+export default function Heesang() {
+	return <SGrid>희상</SGrid>
+}
